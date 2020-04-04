65a095c13c5e2efa6a721b5835cfbaf5e05f708b
Alan Adamiak <alan.adamiak@gmail.com>
2020-04-04 20:52:37 +0200

    Ajout commentaires pour le preprocessing

--- starting_kit/ECOLO1_code_submission/preprocess.py
+++ starting_kit/ECOLO1_code_submission/preprocess.py
@@ -12,12 +12,23 @@ with warnings.catch_warnings():
     from sklearn.feature_selection import SelectKBest  # Used for feature selection
     from sklearn.feature_selection import chi2  # Used for feature selection
     from libscores import get_metric
+    from sklearn.metrics import make_scorer
+    from sklearn.model_selection import cross_val_score
 
 
 class Preprocessor(BaseEstimator):
+    """Contain all methods of preprocessing :
+        - fit and transform the data with default parameters
+        - find the best params
+        - search the best features number
+        - search the best dimensions number
+        - detect and remove outliers
+        - apply the parameters to the data
+    """
+
     def __init__(self):
-        self.best_features_nb = 199
-        self.best_dim_nb = 10
+        self.best_features_nb = 199  # Best number of features found with find_best_features
+        self.best_dim_nb = 10  # Best number of dimension found with find_best_pca
         self.skbest = SelectKBest(chi2, k=self.best_features_nb)
         self.pca = PCA(n_components=self.best_dim_nb)
         self.features_scores = []
@@ -25,21 +36,47 @@ class Preprocessor(BaseEstimator):
         self.detected_outliers = []
 
     def fit(self, X, y):
-        return self.pca.fit(self.skbest.fit(X, y), y)
+        """
+        Run score function on (X, y) and get the appropriate features and dimension.
+        :param X: The input samples.
+        :param y: The target values (class labels).
+        :return: A new version of this instance with fitted pca et skbest
+        """
+        self.skbest = self.skbest.fit(X, y)
+        self.pca = self.pca.fit(X, y)
+        return self
 
     def fit_transform(self, X, y=None):
+        """
+        Fit to data, then transform it.
+        :param X: The imput samples
+        :param y: The target values (class labels).
+        :return: Transformed array.
+        """
         return self.pca.fit_transform(self.skbest.fit_transform(X, y), y)
 
     def transform(self, X, y=None):
+        """
+        Reduce X to the selected features and dimensions.
+        :param X: The imput samples
+        :param y: The target values (class labels).
+        :return: The input samples with only the selected features and dimensions.
+        """
         return self.pca.transform(self.skbest.transform(X))
 
-    # DO NOT USE THIS FUNCTION UNLESS YOU HAVE A POWERFULL COMPUTER OR/AND A LOT OF TIME OR USE A HIGH SPEED (>4)
     def find_best_params(self, speed):
+        """
+        Search the best dimensions and features number.
+        DO NOT USE THIS FUNCTION UNLESS YOU HAVE A POWERFULL COMPUTER OR/AND A LOT OF TIME OR USE A HIGH SPEED (>4)
+        :param speed: The number of features and dimension jumped on each loop
+        :return: The best dimensions and features number
+        """
         print(speed)
         scores = [[0] * 200] * 200
-        Y_train = D.data['Y_train']
+        Y_train = D.data['Y_train'].ravel()
         for i in range(1, 200, speed):
-            M = model(classifier=DecisionTreeClassifier(max_depth=10, max_features='sqrt', random_state=42))
+            # M = model selected by the modeling team
+            M = model(classifier=RandomForestClassifier(n_estimators=116, max_depth=None, min_samples_split=2, random_state=1))
             feature_selection = SelectKBest(chi2, k=i)
             feature_selection.fit(D.data['X_train'], Y_train)
             X_train = feature_selection.transform(D.data['X_train'])
@@ -49,51 +86,73 @@ class Preprocessor(BaseEstimator):
                 pca.fit(D.data['X_train'], Y_train)
                 X_train = pca.transform(D.data['X_train'])
                 tmpM.fit(X_train, Y_train)
-                Y_hat_train = tmpM.predict(X_train)
                 metric_name, scoring_function = get_metric()
-                scores[i][j] = (scoring_function(Y_train, Y_hat_train))
+                scrs = cross_val_score(M, X_train, Y_train, cv=5, scoring=make_scorer(scoring_function))
+                scores[i][j] = (scrs.mean())
         max_pos = np.argmax(scores)
         self.best_features_nb = max_pos // 200
         self.best_dim_nb = max_pos % 200
         print(self.best_features_nb, self.best_dim_nb)
 
     def find_best_features(self):
+        """
+        Execute the model with different quantity of features (1 to 200) and return the quantity of features who give the best model's score.
+        :return: The best features number
+        """
         for i in range(1, 200, 1):
-            M = model(classifier=DecisionTreeClassifier(max_depth=10, max_features='sqrt', random_state=42))
+            M = model(classifier=RandomForestClassifier(n_estimators=116, max_depth=None, min_samples_split=2, random_state=1))
             feature_selection = SelectKBest(chi2, k=i)  # after few tests, 170 look like the best sample
             feature_selection.fit(D.data['X_train'], D.data['Y_train'])
             X_train = feature_selection.transform(D.data['X_train'])
-            Y_train = D.data['Y_train']
+            Y_train = D.data['Y_train'].ravel()
             M.fit(X_train, Y_train)
-            Y_hat_train = M.predict(X_train)
             metric_name, scoring_function = get_metric()
-            self.features_scores.append(scoring_function(Y_train, Y_hat_train))
+            scores = cross_val_score(M, X_train, Y_train, cv=5, scoring=make_scorer(scoring_function))
+            self.features_scores.append(scores.mean())
         self.best_features_nb = self.features_scores.index(max(self.features_scores))
 
     def detect_outliers(self):
+        """
+        Detect the outliers from the data and mark down who they are.
+        :return: A list containing 1 if the element is not an outlier, -1 else.
+        """
         rng = np.random.RandomState(42)
-        clf = IsolationForest(max_samples=10000, random_state=rng)  # IsolationForest is a choice
-        clf.fit(D.data['X_train'])
-        self.detected_outliers = clf.predict(D.data['X_train'])
+        clf = IsolationForest(max_samples=10000, random_state=rng)  # We choose IsolationForest after lookings on the documentations of differents type of outliers_detectors methodes.
+        clf.fit(D.data['X_train'])  # use IsolationForest on the datas
+        self.detected_outliers = clf.predict(D.data['X_train']) # mark down the outilers of the datas (detected_outliers= -1 if outlier, 1 else )
 
     def removeOutliers(self, y, data):
+        """
+        Remove the ouliers who are detected
+        :param y: The list of detected outliers
+        :param data: The data of which the outliers will be removed
+        :return: The data without outliers
+        """
         return data[y > 0]
 
     def find_best_pca(self):
+        """
+        Find the best dimensions number using the PCA (Principal Component Analysis).
+        :return: The best dimensions number
+        """
         scores = []
         for i in range(1, 200, 1):
-            M = model(classifier=DecisionTreeClassifier(max_depth=10, max_features='sqrt', random_state=42))
+            M = model(classifier=RandomForestClassifier(n_estimators=116, max_depth=None, min_samples_split=2, random_state=1))
             pca = PCA(n_components=i)
             pca.fit(D.data['X_train'], D.data['Y_train'])
             X_train = pca.transform(D.data['X_train'])
-            Y_train = D.data['Y_train']
+            Y_train = D.data['Y_train'].ravel()
             M.fit(X_train, Y_train)
-            Y_hat_train = M.predict(X_train)
             metric_name, scoring_function = get_metric()
-            self.pca_scores.append(scoring_function(Y_train, Y_hat_train))
+            scores = cross_val_score(M, X_train, Y_train, cv=5, scoring=make_scorer(scoring_function))
+            self.pca_scores.append(scores.mean())
         self.best_dim_nb = self.pca_scores.index(max(self.pca_scores))
 
     def apply_parameters(self):
+        """
+        Apply to all data what have been fund as best features and pca and detect and remove the outliers
+        :return: Preprocessed data
+        """
         feature_selection = SelectKBest(chi2, k=self.best_features_nb).fit(D.data['X_train'], D.data['Y_train'])
         # apply this feature_selection to the data
         D.data['X_train'] = feature_selection.transform(D.data['X_train'])
@@ -107,12 +166,20 @@ class Preprocessor(BaseEstimator):
         D.data['X_test'] = pca.transform(D.data['X_test'])
 
     def preprocess(self):
+        """
+        Call all the methods in one
+        :return: Preprocessed data
+        """
         self.find_best_features()
         self.find_best_pca()
         self.detect_outliers()
         self.apply_parameters()
 
     def plots(self):
+        """
+        Visualize the results of the preprocessing
+        :return: 3 plots
+        """
         plt.plot(self.features_scores)
         plt.plot(self.pca_scores)
         sns.scatterplot(x="sum_axis_1_50", y="variance", data=data, hue=y_pred_train)
@@ -126,7 +193,7 @@ if __name__ == "__main__":
         output_dir = "../results"
     else:
         input_dir = argv[1]
-        output_dir = argv[2];
+        output_dir = argv[2]
 
     basename = 'Iris'
     D = DataManager(basename, input_dir)  # Load data
